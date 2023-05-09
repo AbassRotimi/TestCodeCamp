82bf77e29abf10af5fd6f4354ee0b6cf123c3a33
AbassRotimi <abassrotimi@gmail.com>
2023-05-09 15:44:24 +0100

    Added Task 14 again

---
+++ FirstJavaProject/FirstJavaProject/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World! This is my first Java Project");
+        System.out.println("My favourite food is Pounded Yam and Efo riro");
+        System.out.println("Life is good");
+        System.out.println("The sun shines bright");
+        System.out.println("It is raining in Ibadan currently");
+    }
+}
